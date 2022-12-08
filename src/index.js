26d9b3c3002adae4eb64d3f009d3ec1f9f9eae2f
Leonardo Jesus <leonardojesus02@outlook.com>
2022-12-07 21:07:32 -0300

    feat: add dotenv to secure secrets and envexample

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-require('./models/User'); 
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
+require('dotenv').config();
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
-const mongoUri = HIDDEN for GitHub;
+
+const mongoUri = proccess.env.MONGO_URI;
+
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true
 });
-
 mongoose.connection.on('connected', () => {
     console.log("Connected to mongo instance");
-})
+});
 mongoose.connection.on('error', (err) => {
     console.error("Error connecting to mongo", err);
 });
 
-app.get('/', (req, res) => {
-    res.send("The Express request works for the Tracker App!")
+app.get('/', (_req, res) => {
+    res.send("The Express request works for the Tracker App!");
 });
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`✔ Listening on port ${port}...`));
